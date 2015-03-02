0dd6483bf7d9e655040268ed3b63a7e305b44b63
William Surles <williamsurles@gmail.com>
2015-03-02 08:02:34 -0700

    added a new bar chart to explore categories, added chart size to bootstrap file

--- explore/explore_transactions.R
+++ explore/explore_transactions.R
@@ -11,42 +11,52 @@ str(df_trans)
 str(df2)
 head(df2,20)
 
-crunch_trans <- function(df_trans) {
-  
-  df2 <- df_trans %>%
-    filter(!(Category %in% c('Transfer','Credit Card Payment',
-                             'Hide from Budgets & Trends',
-                             'Transfer for Cash Spending',
-                             'Cash & ATM','Withdrawal',
-                             'Sell','Buy','Deposit',
-                             'Federal Tax', 'State Tax'))) %>%       
-    filter(!(Category %in% c('Bonus', 'Interest Income', 'Income',
-                             'Paycheck', 'Reimbursement', 
-                             'Rental Income', 'Returned Purchase', 
-                             'Credit Card Cashback', 'Gift Received', 
-                             'Side Job', 'Dividend & Cap Gains'))) %>%
-    mutate(
-      date = as.Date(Date, format = "%m/%d/%Y"),
-      date_str = as.character(date),
-      year = year(date),
-      yday = yday(date),
-      month = month(date),
-      mday = mday(date),
-      Amount = ifelse(Transaction.Type == 'credit', Amount * -1, Amount)
-    ) %>%
-    group_by(year, Category) %>%
-    summarize(
-      sum_amount = sum(Amount, na.rm = T),
-      count = n()
-    ) %>%
-    group_by() %>%
-    mutate(
-      sum_amount_pretty = paste0('$', prettyNum(round(sum_amount), big.mark=",",scientific=F))
-      ) %>%
-    data.frame()
 
-  return(df2)
-}
+
+df2 <- df_trans %>%
+  filter(!(Category %in% c('Transfer','Credit Card Payment',
+                           'Hide from Budgets & Trends',
+                           'Transfer for Cash Spending',
+                           'Cash & ATM','Withdrawal',
+                           'Sell','Buy','Deposit',
+                           'Federal Tax', 'State Tax'))) %>%       
+  filter(!(Category %in% c('Bonus', 'Interest Income', 'Income',
+                           'Paycheck', 'Reimbursement', 
+                           'Rental Income', 'Returned Purchase', 
+                           'Credit Card Cashback', 'Gift Received', 
+                           'Side Job', 'Dividend & Cap Gains'))) %>%
+  mutate(
+    date = as.Date(Date, format = "%m/%d/%Y"),
+    date_str = as.character(date),
+    year = year(date),
+    yday = yday(date),
+    month = month(date),
+    mday = mday(date),
+    Amount = ifelse(Transaction.Type == 'credit', Amount * -1, Amount)
+  ) %>%
+  group_by(year, Category) %>%
+  summarize(
+    sum_amount = sum(Amount, na.rm = T),
+    count = n()
+  ) %>%
+  data.frame()
+
+df_expand <- expand.grid(year = unique(df2$year), Category = unique(df2$Category)) %>%
+  mutate(Category = as.character(Category)) %>%
+  data.frame()
+
+df4 <- left_join(df_expand, df2, by=c("year","Category"))
+df4[is.na(df4)] <- 0
+df5 <- df4 %>%
+  group_by(Category) %>%
+  mutate(total = sum(sum_amount)) %>%
+  group_by() %>%
+  arrange(desc(total)) %>%
+  mutate(
+    sum_amount_pretty = paste0('$', prettyNum(round(sum_amount), big.mark=",",scientific=F))
+  ) %>%
+  data.frame()
+
 
 df2 <- crunch_trans(df_trans)
 str(df2)
@@ -56,9 +66,6 @@ p <- nPlot(sum_amount ~ count, group = 'year', data = df2, type = 'scatterChart'
 p$xAxis(axisLabel = 'Number of Transactions')
 p$yAxis(axisLabel = 'Total Spending')
 p$chart(size = '#! function(d){return d.sum_amount} !#')
-# p$xAxis(tickFormat="#! function(d) {return Math.round(Math.pow(10, d));}!#")
-# p$yAxis(tickFormat="#! function(d) {return Math.round(Math.pow(10, d));}!#")
-# p$chart(color = color_module)
 p$chart(tooltipContent = "#!
         function(key, x, y, d){ 
           return '<h3>' + d.point.Category + '</h3>' +
@@ -69,4 +76,9 @@ p$chart(tooltipContent = "#!
         }
         !#")
 p
+
+##| Create plot
+p2 <- nPlot(sum_amount ~ Category, group = 'year', data = df5, type = "multiBarHorizontalChart")
+p2$chart(margin = list(top = 100, right = 20, bottom = 50, left = 180), height=2000, width=800)
+p2
 
